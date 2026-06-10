d0bef51480b0cb5b79d92003b2f8e1c0728f7daf
Rgpraveen11 <gurupraveen2533@gmail.com>
2026-06-10 14:48:15 +0530

    AI Resume Builder Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Homepage from './pages/Home'
+function App() {
+
+
+  return (
+    <>
+    {/* <h1>Resume builder</h1> */}
+      <Homepage />
+    </>
+  )
+}
+
+export default App
